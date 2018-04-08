abb24cad8a71bd5a2129e177ab02495b3b9a91fe
Keila Martínez <kmartin5@eafit.edu.co>
2018-04-07 22:18:24 -0500

    Vista no funcional de minijuegos

---
+++ Cliente/Assets/Scripts/Vista/VistaMinijuegos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VistaMinijuegos : MonoBehaviour {
+
+    public GameObject table;
+    public GameObject timer;
+    public GameObject category;
+    public GameObject HUnumber;
+    public GameObject continuar;
+    public float targetTime = 120.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer();
+	}
+
+    void Continuar() {
+
+    }
+
+    void Timer() {
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f) {
+
+            timerEnded();
+        }
+    }
+    void timerEnded()
+    {
+       //UIText timerText = Timer.Get
+        //mer.text = "tiempo";
+    }
+}
