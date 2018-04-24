7452230ca850f60ed887f6a25ce7daf5603c8995
Edison Zapata <ezapata7@eafit.edu.co>
2018-04-24 06:22:37 -0500

    Estructura base del cliente movil y vista unirse a partida

---
+++ ClienteMovil/Assets/Scripts/Keypad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour {
+    
+    public Text codigo;
+    int[] word = new int[]{-2, -2, -2, -2, -2, -2};
+    int cursor = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string aux = "";
+		for(int i=0; i<6; i++) {
+            if(i==3)
+                aux += "- ";
+            if(word[i]!=-2) {
+                aux += word[i] + " ";
+            } else {
+                aux += "_ ";
+            }
+        }
+        codigo.text = aux;
+	}
+
+    public void keyPress(int key) {
+        if(key>=0 && key<=9 && cursor<6) {
+            word[cursor] = key;
+            cursor+=1;
+        }
+        if(key==-1 && cursor>0) {
+            word[cursor-1] = -2;
+            cursor-=1;
+        } 
+        if(key==10) {
+            //enviar codigo
+        }
+    }
+}
