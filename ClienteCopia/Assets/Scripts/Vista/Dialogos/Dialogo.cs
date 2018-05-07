1cd0aec82273b72699256b12758b29ec39956acd
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2018-05-07 13:33:08 -0500

    Eliminando copia de cliente.

--- ClienteCopia/Assets/Scripts/Vista/Dialogos/Dialogo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class Dialogo {
-    [TextArea(2,5)]
-    public string [] lineasDeDialogo;
-}
