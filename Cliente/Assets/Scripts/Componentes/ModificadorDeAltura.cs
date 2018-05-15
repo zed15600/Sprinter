3890f98d90c8793e985cc4b50b4b8ece3cbbe987
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2018-05-15 01:23:33 -0500

    Se modifica el sistema de dialogos y se agregan más dialogos.

---
+++ Cliente/Assets/Scripts/Componentes/ModificadorDeAltura.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModificadorDeAltura : MonoBehaviour {
+
+    public GameObject contenidoAModificar;
+    public VerticalLayoutGroup alturaACopiar;
+
+    public void cambiarAltura() {
+        float altura = 0;
+        Text[] hijos = alturaACopiar.GetComponentsInChildren<Text>(false);
+        foreach (Text hijo in hijos) {
+            altura += hijo.gameObject.transform.localScale.y;
+        }
+        contenidoAModificar.transform.localScale = new Vector3
+            (contenidoAModificar.transform.localScale.x,
+             altura,
+             contenidoAModificar.transform.localScale.z);
+    }
+
+    void OnEnable() {
+        cambiarAltura();
+    }
+}
