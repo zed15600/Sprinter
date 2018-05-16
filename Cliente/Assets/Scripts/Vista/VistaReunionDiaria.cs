039f1422fb4d3eb4c416c2eac6dcb06decd396d1
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2018-05-16 14:19:05 -0500

    Se inicializan los elementos a ser usados en la vista reunion.

--- Cliente/Assets/Scripts/Vista/VistaReunionDiaria.cs
+++ Cliente/Assets/Scripts/Vista/VistaReunionDiaria.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VistaReunionDiaria : ClientElement {
 
+    public Text Historia;
+    public Text diaSprint;
+
+    public VerticalLayoutGroup criteriosLayout;
+    public GridLayoutGroup jugadoresEnProblemas;
+
+    public GameObject jugadoresPrefab;
+    public GameObject criteriosPrefab;
+    
+    
 	void OnEnable () {
         controlador.cargarDialogoGlobal(5);	
 	}
